094f944c14adac91ea8876492166f8dd0658a5b0
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-02-01 15:13:27 +0530

    Login page completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  public loginForm !: FormGroup;
+
+  constructor(private _fb: FormBuilder, private _http: HttpClient, private _router: Router) {}
+
+  ngOnInit(): void {
+      this.loginForm = this._fb.group ({
+        email: [''],
+        password: ['']
+      })
+  }
+
+  login() {
+    this._http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res => {
+      const user = res.find((a: any)=> {
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user) {
+        alert("Login Success");
+        this.loginForm.reset();
+        this._router.navigate(['dashboard']);
+      } else {
+        alert("User not found!!");
+      }
+    },
+    err => {
+      alert("Something went wrong!!");
+    });
+  }
 }
